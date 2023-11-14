b4230f48993c12ac1476eccf9eaba8900e15d049
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2023-11-14 10:21:51 +0100

    add title and icon

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,9 +1,13 @@
+import { MovieIcon } from '../icons/MovieIcon'
 import './Header.css'
 
 export default function Header() {
 	return (
 		<div className='header'>
-			<h1>Movies Search</h1>
+			<div className='title'>
+				<MovieIcon />
+				<h1>Movies Search</h1>
+			</div>
 			<form>
 				<input type='text' placeholder='Avengers, Star Wars...' />
 				<button type='submit'>SEARCH</button>
