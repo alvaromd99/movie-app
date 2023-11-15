26bc3076b562b56e89a86550e67c617e47d8b074
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2023-11-15 10:58:05 +0100

    change api names

--- src/components/movies/MovieCard.jsx
+++ src/components/movies/MovieCard.jsx
@@ -3,9 +3,9 @@ import './MovieCard.css'
 export default function MovieCard({ movie }) {
 	return (
 		<div className='card'>
-			<img src={movie.Poster} alt={`${movie.Title} poster`} />
-			<h3>{movie.Title}</h3>
-			<p>{movie.Year}</p>
+			<img src={movie.poster} alt={`${movie.title} poster`} />
+			<h3>{movie.title}</h3>
+			<p>{movie.year}</p>
 		</div>
 	)
 }
--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -4,10 +4,20 @@ import MovieCard from './MovieCard'
 
 export default function Movies() {
 	const movies = data.Search
+
+	// Avoid using apis names
+	const mappedMovies = movies.map((m) => ({
+		title: m.Title,
+		year: m.Year,
+		id: m.imdbID,
+		type: m.Type,
+		poster: m.Poster,
+	}))
+
 	return (
 		<div className='movies-cont'>
-			{movies.map((movie) => (
-				<MovieCard key={movie.imdbID} movie={movie} />
+			{mappedMovies.map((movie) => (
+				<MovieCard key={movie.id} movie={movie} />
 			))}
 		</div>
 	)
