cd093508212c3a7c4cbe5db9462b5b42265e0a14
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2023-11-20 10:07:17 +0100

    avoid re searching the films

--- src/hooks/UseMovies.js
+++ src/hooks/UseMovies.js
@@ -1,15 +1,29 @@
 import { useState } from 'react'
 import { searchMovies } from '../services/movies'
+import { useRef } from 'react'
 
 export function UseMovies({ query }) {
 	const [responseMovies, setResponseMovies] = useState([])
 	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState(null)
+	const previousSearch = useRef(query)
 
 	const getMovies = async () => {
-		setLoading(true)
-		const newMovies = await searchMovies({ query })
-		setLoading(false)
-		setResponseMovies(newMovies)
+		// Avoid searching the movies again if query didn`t change
+		if (query === previousSearch.current) return
+
+		try {
+			setLoading(true)
+			setError(null)
+			previousSearch.current = query
+			const newMovies = await searchMovies({ query })
+			setResponseMovies(newMovies)
+		} catch (error) {
+			setError(error.message)
+		} finally {
+			setLoading(false)
+		}
 	}
-	return { responseMovies, loading, getMovies }
+
+	return { responseMovies, loading, error, getMovies }
 }
